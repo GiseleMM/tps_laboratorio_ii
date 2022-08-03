1b188474980e41277c5651c9fa23106574184a33
gisele <giselemedina01@gmail.com>
2022-08-02 21:46:32 -0300

    resubiendo Tp3

---
+++ RecuperatoriosTP/TP-3/Medina.Gisele.2A.TPFinal/GMAdopcion/FrmAltaAdoptante.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using System.Threading;
+namespace GMAdopcion
+{
+    public partial class FrmAltaAdoptante : Form,IActualizarDatos,IValidarCampos,IObtenerDatos<Adoptante>
+    {
+        Adoptante adoptanteAux = null;
+        public FrmAltaAdoptante()
+        {
+            InitializeComponent();
+            ActualizarDatos();
+           
+        }
+
+        public void ActualizarDatos()
+        {
+            this.comboBox_localidades.DataSource = null;
+            foreach (ELocalidad item in Enum.GetValues(typeof(ELocalidad)))
+            {
+                this.comboBox_localidades.Items.Add(item);
+            }
+        }
+
+        public Adoptante ObtenerDatos()
+        {
+            return adoptanteAux;
+        }
+
+  
+        public void ValidarCampos()
+        {   if((textBox_nombre.Text==string.Empty) || (textBox_direccion.Text==string.Empty) || textBox_dni.Text== string.Empty || textBox_telefono.Text==string.Empty)
+            {
+                throw new CargaDeDatosExceptions("comppletar todos los datos requeridos");
+            }
+            if(!int.TryParse(this.textBox_dni.Text, out int dni) || (int.Parse(this.textBox_dni.Text) <= 0 || int.Parse(this.textBox_dni.Text) >= 99999999))
+            {
+                throw new CargaDeDatosExceptions("Error cargar de dni");
+            }
+            if(!int.TryParse(this.textBox_telefono.Text, out int telefono) || textBox_telefono.Text.Length < 0 || textBox_telefono.Text.Length > 10 || telefono < 0)
+            {
+                throw new CargaDeDatosExceptions("Error en carga de telefono");
+            }
+        
+        
+        }
+
+     
+
+        private void button_aceptar_Click_1(object sender, EventArgs e)
+        {
+            int dalay = 2000;
+            string nombre;
+            int dni;
+            string direccion;
+            int telefono;
+            try
+            {
+                ValidarCampos();
+                nombre = this.textBox_nombre.Text;
+                direccion = this.textBox_direccion.Text;
+                dni = int.Parse(this.textBox_dni.Text);
+                telefono = int.Parse(this.textBox_telefono.Text);
+                adoptanteAux = new Adoptante(nombre, direccion, dni, telefono);
+
+                MessageBox.Show("Procesando datos......");
+                Thread.Sleep(dalay);
+                if (MessageBox.Show(adoptanteAux.ToString(), "confirma?", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    MessageBox.Show("Se evaluaran la validez datos antes de permitir la adopcion");
+
+
+                    this.DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    textBox_direccion.Text = String.Empty;
+                    textBox_dni.Text = string.Empty;
+                    textBox_nombre.Text = string.Empty;
+                    textBox_telefono.Text = string.Empty;
+                }
+            }
+            catch (CargaDeDatosExceptions ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\nError alta Perro, Cargar de datos");
+
+            }
+        }
+
+        private void button_cancelar_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP-3/Medina.Gisele.2A.TPFinal/GMAdopcion/FrmInicio.cs
@@ -0,0 +1,133 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+namespace GMAdopcion
+{
+    public partial class FrmInicio : Form, IActualizarDatos
+    {
+        List<Perro> listaPerros;
+        List<Adoptante> listaAdoptantes;
+        Perro seleccionDelPerro;
+        Adoptante seleccionAdoptante;
+        public FrmInicio()
+        {
+            InitializeComponent();
+            listaPerros = ClaseSerializadora<List<Perro>>.DeSerializar_json("listaPerros.json", EDirectorio.DirectorioBaseProyecto);
+            listaAdoptantes = ClaseSerializadora<List<Adoptante>>.DeSerializar_json("listaAdoptantes.json", EDirectorio.DirectorioBaseProyecto);
+            seleccionDelPerro = null;
+            seleccionAdoptante = null;
+
+
+            ActualizarDatos();
+            this.textBox_adoptante.Text = "";
+            this.textBox_perro.Text = "";
+
+
+        }
+        public void ActualizarDatos()
+        {
+
+            this.comboBox_Perros.DataSource = null;
+            this.comboBox_Perros.DataSource = listaPerros;
+            this.comboBox_Perros.SelectionStart = 0;
+            this.comboBox_adoptantes.DataSource = null;
+            this.comboBox_adoptantes.DataSource = listaAdoptantes;
+            this.comboBox_adoptantes.SelectionStart = 0;
+        }
+
+
+
+        private void comboBox_Perros_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seleccionDelPerro = (Perro)this.comboBox_Perros.SelectedItem;
+            if (seleccionDelPerro is not null)
+            {
+                if (!seleccionDelPerro.ParaAdopcion)
+                {
+                    seleccionDelPerro = null;
+                    this.textBox_perro.Text = string.Empty;
+                }
+                else
+                {
+
+
+                    this.textBox_perro.Text = seleccionDelPerro.Nombre;
+
+                }
+
+            }
+
+
+        }
+
+
+        private void button_agregarAdoptante_Click(object sender, EventArgs e)
+        {
+            FrmAltaAdoptante frmAltaAdoptante = new FrmAltaAdoptante();
+            frmAltaAdoptante.ShowDialog();
+            if (frmAltaAdoptante.DialogResult == DialogResult.OK)
+            {
+                Adoptante aux = frmAltaAdoptante.ObtenerDatos();
+                string ruta = Archivo.Guardar($"Adoptante_{DateTime.Now.ToString("HH-mm-ss")}.txt", aux.ToString(), EDirectorio.DirectorioBaseProyecto);
+                MessageBox.Show($"Se a guardado dato de posible Adoptante en {ruta}");
+            }
+        }
+
+        private void button_AgregarPerros_Click(object sender, EventArgs e)
+        {
+            FrmAltaPerro frmAltaPerro = new FrmAltaPerro();
+            frmAltaPerro.ShowDialog();
+            if (frmAltaPerro.DialogResult == DialogResult.OK)
+            {
+                listaPerros.Add(frmAltaPerro.ObtenerDatos());
+                ActualizarDatos();
+
+
+            }
+        }
+
+        private void button_guardar_Click(object sender, EventArgs e)
+        {
+            
+            ClaseSerializadora<List<Perro>>.Serializar_json(listaPerros, "listaPerros.json", EDirectorio.DirectorioBaseProyecto);
+            ClaseSerializadora<List<Adoptante>>.Serializar_json(listaAdoptantes, "listaAdoptantes.json", EDirectorio.DirectorioBaseProyecto);
+            MessageBox.Show($"listaPerros.json  y listaAdoptante.json guardada en {AppDomain.CurrentDomain.BaseDirectory}");
+
+        }
+
+        private void comboBox_adoptantes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seleccionAdoptante = (Adoptante)this.comboBox_adoptantes.SelectedItem;
+
+            if (seleccionAdoptante is null)
+            {
+
+                this.textBox_adoptante.Text = string.Empty;
+            }
+            else
+            {
+
+
+                this.textBox_adoptante.Text = seleccionAdoptante.Nombre;
+
+            }
+
+        }
+
+        private void button_si_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox_adoptante.Text) && !string.IsNullOrEmpty(textBox_perro.Text))
+            {
+
+                seleccionAdoptante += seleccionDelPerro;
+                ActualizarDatos();
+            }
+            else
+            {
+                MessageBox.Show("Debe cargar Campos");
+            }
+        }
+
+    }
+}
